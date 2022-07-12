02ff2c1a5808abb495b3e16f3042074be3f95e03
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-12 23:35:50 +0200

    Remove footer

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 
 type LayoutProps = {
@@ -11,7 +10,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <React.Fragment>
       <Navbar />
       <main>{children}</main>
-      <Footer />
     </React.Fragment>
   );
 };
