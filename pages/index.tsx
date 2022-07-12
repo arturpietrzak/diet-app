161aa1e408e92ed3d738493a5c4c3309c96e4152
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-13 00:03:29 +0200

    Readd all components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
-import Navbar from "../components/layouts/Navbar/NavbarPhone/NavbarPhone";
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
