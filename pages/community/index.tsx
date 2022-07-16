eb8badc59a798b129c90d0d3b8fdc6d726bb97f4
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-17 00:41:00 +0200

    Fix build errors

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Post, { PostProps } from "../../components/community/post/Post";
+import { getRandomInt } from "../../utils/randon";
 
 import styles from "./community.module.scss";
 
@@ -10,44 +11,49 @@ const placeholderPosts: PostProps[] = [
     userName: "Jeremy Buendia",
     text: "Short description of the post",
     imageUrl: "https://picsum.photos/500/500",
-    likes: 11,
-    comments: 3,
+    likes: getRandomInt(0, 10),
+    comments: getRandomInt(0, 10),
+    isLiked: true,
   },
   {
-    id: "1",
+    id: "2",
     userImageUrl: "https://picsum.photos/200/200",
     userName: "1",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa explicabo dolorum voluptatum, saepe adipisci neque cupiditate cumque blanditiis corporis? Eos accusamus soluta aut officiis dicta eaque nemo ad voluptatem facere Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa explicabo dolorum voluptatum, saepe adipisci neque cupiditate cumque blanditiis corporis? Eos accusamus soluta aut officiis dicta eaque nemo ad voluptatem facere",
     imageUrl: "https://picsum.photos/500/500",
-    likes: 11,
-    comments: 3,
+    likes: getRandomInt(0, 10),
+    comments: getRandomInt(0, 10),
+    isLiked: false,
   },
   {
-    id: "1",
+    id: "3",
     userImageUrl: "https://picsum.photos/200/200",
     userName: "1",
     text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
     imageUrl: "https://picsum.photos/500/500",
-    likes: 11,
-    comments: 3,
+    likes: getRandomInt(0, 10),
+    comments: getRandomInt(0, 10),
+    isLiked: false,
   },
   {
-    id: "1",
+    id: "4",
     userImageUrl: "https://picsum.photos/200/200",
     userName: "1",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa explicabo dolorum voluptatum, saepe adipisci neque cupiditate cumque blanditiis corporis? Eos accusamus soluta aut officiis dicta eaque nemo ad voluptatem facere",
     imageUrl: "https://picsum.photos/500/500",
-    likes: 11,
-    comments: 3,
+    likes: getRandomInt(0, 10),
+    comments: getRandomInt(0, 10),
+    isLiked: false,
   },
   {
-    id: "1",
+    id: "5",
     userImageUrl: "https://picsum.photos/200/200",
     userName: "1",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa explicabo dolorum voluptatum, saepe adipisci neque cupiditate cumque blanditiis corporis? Eos accusamus soluta aut officiis dicta eaque nemo ad voluptatem facere",
     imageUrl: "https://picsum.photos/500/500",
-    likes: 11,
-    comments: 3,
+    likes: getRandomInt(0, 10),
+    comments: getRandomInt(0, 10),
+    isLiked: false,
   },
 ];
 
