ee32862d1fc94c9390b6cf6a175156776e812175
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-23 23:36:25 +0200

    Fix chart issues

--- components/diets/Analytics/Analytics.tsx
+++ components/diets/Analytics/Analytics.tsx
@@ -2,8 +2,11 @@ import styles from "./Analytics.module.scss";
 
 import dynamic from "next/dynamic";
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+import { ApexOptions } from "apexcharts";
 
 import { PieChart } from "react-minimal-pie-chart";
+import useWindowSize from "../../../hooks/useWindowSize";
+import useDeviceType from "../../../hooks/useDeviceType";
 
 export type AnalyticsProps = {
   calories: number;
@@ -14,103 +17,114 @@ export type AnalyticsProps = {
 };
 
 const Analytics = (props: AnalyticsProps) => {
-  var today = new Date();
-  var priorDate = new Date(new Date().setDate(today.getDate() - 30));
+  const today = new Date();
+  const priorDate = new Date(new Date().setDate(today.getDate() - 30));
 
-  console.log(priorDate);
+  const series = [
+    {
+      name: "Calories",
+      data: props.pastCalories.map((item) => {
+        return {
+          x: item.dateTime,
 
-  const lineChartData = {
-    series: [
-      {
-        name: "Calories",
-        data: props.pastCalories.map((item) => {
-          return {
-            x: item.dateTime,
+          y: item.calories,
+        };
+      }),
+    },
+  ];
 
-            y: item.calories,
-          };
-        }),
-      },
-    ],
-
-    options: {
-      chart: {
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
+  const options: ApexOptions = {
+    chart: {
+      zoom: {
         enabled: false,
       },
-      markers: {
-        size: 0,
-        style: "hollow",
+      toolbar: {
+        show: false,
       },
-      xaxis: {
-        type: "datetime",
-        min: priorDate.getTime(),
-        tickAmount: 6,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+      size: 0,
+    },
+    xaxis: {
+      type: "datetime",
+      min: priorDate.getTime(),
+      tickAmount: 6,
+    },
+    tooltip: {
+      x: {
+        format: "dd MMM yyyy",
       },
-      tooltip: {
-        x: {
-          format: "dd MMM yyyy",
-        },
-        theme: "dark",
-        style: {
-          fontSize: "1rem",
-        },
+      theme: "dark",
+      style: {
+        fontSize: "1rem",
       },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shadeIntensity: 1,
-          opacityFrom: 0.8,
-          opacityTo: 0.3,
-          stops: [0, 100],
-        },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 0.8,
+        opacityTo: 0.3,
+        stops: [0, 100],
       },
     },
   };
 
-  return (
-    <div className={styles.analytics}>
-      <div className={styles.macro}>
-        <div className={styles.numbers}>
-          <h3 className={styles.header}>Analytics</h3>
-          <div className={styles.legend}>
-            <div className={styles.carbs}>Fats (grams)</div>
-            <div className={styles.fat}>Protein (grams)</div>
-            <div className={styles.protein}>Carbs (grams)</div>
+  if (true) {
+    return (
+      <div className={styles.analytics}>
+        <div className={styles.macro}>
+          <div className={styles.numbers}>
+            <h3 className={styles.header}>Analytics</h3>
+            <div className={styles.legend}>
+              <div className={styles.carbs}>Fats (grams)</div>
+              <div className={styles.fat}>Protein (grams)</div>
+              <div className={styles.protein}>Carbs (grams)</div>
+            </div>
+          </div>
+          <div className={styles.pieChart}>
+            <PieChart
+              animate
+              animationEasing="ease-out"
+              label={({ dataEntry }) => dataEntry.value}
+              labelStyle={{
+                fontSize: "8px",
+                fill: "#FFFFFF",
+                fontWeight: "400",
+              }}
+              data={[
+                {
+                  title: "Carbohydrates",
+                  value: props.carbs,
+                  color: "#4287f5",
+                },
+                { title: "Fat", value: props.fat, color: "#d6410f" },
+                { title: "Protein", value: props.protein, color: "#6A2135" },
+              ]}
+              lineWidth={35}
+              radius={50}
+              labelPosition={82}
+            />
+            <div className={styles.pieChartCalories}>
+              <span className={styles.number}>{props.calories}</span>
+              <span className={styles.label}>kcal</span>
+            </div>
           </div>
         </div>
-        <div className={styles.pieChart}>
-          <PieChart
-            animate
-            animationEasing="ease-out"
-            label={({ dataEntry }) => dataEntry.value}
-            labelStyle={{
-              fontSize: "8px",
-              fill: "#FFFFFF",
-              fontWeight: "400",
-            }}
-            data={[
-              { title: "Carbohydrates", value: props.carbs, color: "#4287f5" },
-              { title: "Fat", value: props.fat, color: "#d6410f" },
-              { title: "Protein", value: props.protein, color: "#6A2135" },
-            ]}
-            lineWidth={35}
-            radius={50}
-            labelPosition={82}
+        <div className={styles.chartContainer}>
+          <ApexCharts
+            height={300}
+            type="area"
+            series={series}
+            options={options}
           />
-          <div className={styles.pieChartCalories}>
-            <span className={styles.number}>{props.calories}</span>
-            <span className={styles.label}>kcal</span>
-          </div>
         </div>
       </div>
-      <ApexCharts {...lineChartData} type={"area"} width={700} height={300} />
-    </div>
-  );
+    );
+  }
 };
 
 export default Analytics;
