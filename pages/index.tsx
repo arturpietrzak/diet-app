5c4f38041d29ebf41d2d76458c3d59b0356ddda6
ArtJSON <artur.pietrzak1@outlook.com>
2022-07-25 00:07:20 +0200

    Add cards for different plans of the products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import styles from "./index.module.scss";
-import Image from "next/image";
+import Image from "next/future/image";
 import useDeviceType from "../hooks/useDeviceType";
 
 import backgroundMobile from "../public/images/main-img-phone.png";
 import backgroundTablet from "../public/images/main-img-tablet.png";
 import backgroundDesktop from "../public/images/main-img-desktop.png";
+import sectionImg from "../public/images/section-img.png";
 
 const getBackgroundImage = (deviceType: string) => {
   switch (deviceType) {
@@ -42,7 +43,6 @@ const Home: NextPage = () => {
         <div className={styles.imageContainer}>
           <Image
             className={styles.image}
-            layout="fill"
             src={getBackgroundImage(deviceType)}
             priority
             alt="diet"
@@ -50,6 +50,31 @@ const Home: NextPage = () => {
           />
         </div>
       </header>
+      <section className={styles.purchaseSection}>
+        <div className={styles.section}>
+          <div className={styles.cards}>
+            <div className={styles.card}>
+              <h3 className={styles.planName}>Basic plan</h3>
+              <ul>
+                <li>Personalized diet</li>
+                <li>Access to the community</li>
+                <li>1-on-1 consultations with dietician</li>
+              </ul>
+              <button>Choose this plan</button>
+            </div>
+            <div className={`${styles.card} ${styles.premium}`}>
+              <h3 className={styles.planName}>Premium plan</h3>
+              <ul>
+                <li>Personalized diet</li>
+                <li>Access to the community</li>
+                <li>1-on-1 consultations with dietician</li>
+                <li>Training plan with instructions</li>
+              </ul>
+              <button>Choose this plan</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
